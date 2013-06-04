5216e3362c7aa7b2f021f621768e23b2e9cdae7e
Xia Zhiyong <brave1024@gmail.com>
2013-06-05 00:19:27 +0800

    add a jailbreak target

---
+++ videoMonitor/videoMonitor-JBTests/videoMonitor_JBTests.h
@@ -0,0 +1,13 @@
+//
+//  videoMonitor_JBTests.h
+//  videoMonitor-JBTests
+//
+//  Created by Xia Zhiyong on 13-6-5.
+//  Copyright (c) 2013年 Xia Zhiyong. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface videoMonitor_JBTests : SenTestCase
+
+@end
---
+++ videoMonitor/videoMonitor-JBTests/videoMonitor_JBTests.m
@@ -0,0 +1,32 @@
+//
+//  videoMonitor_JBTests.m
+//  videoMonitor-JBTests
+//
+//  Created by Xia Zhiyong on 13-6-5.
+//  Copyright (c) 2013年 Xia Zhiyong. All rights reserved.
+//
+
+#import "videoMonitor_JBTests.h"
+
+@implementation videoMonitor_JBTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in videoMonitor-JBTests");
+}
+
+@end
